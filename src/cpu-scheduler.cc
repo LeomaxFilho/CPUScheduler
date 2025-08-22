60bce6e8a2080eeae4e5a0bde9b190f4facf0b27
luis <luisalves2506@outlook.com>
2025-08-22 17:25:25 -0300

    add: SJF and Read File

--- src/cpu-scheduler.cc
+++ src/cpu-scheduler.cc
@@ -22,5 +22,56 @@ void CPUScheduler::showResults(const string algorithm, const vector<Process> pro
     averageWaitTimeProcess /= vectorSize;
     averageReturnTimeProcess /= vectorSize;
 
-    cout << fixed << setprecision(1) << algorithm << averageReturnTimeProcess << " " << averageResponseTimeProcess << " " << averageWaitTimeProcess;
+    cout << fixed << setprecision(1) << algorithm << averageReturnTimeProcess << " " << averageResponseTimeProcess << " " << averageWaitTimeProcess << endl;
+}
+
+void CPUScheduler::SJF(){
+   
+    // Copia do vetor de processos para nao realizar nenhuma operacao com ele
+    vector<Process> procs = this->process;
+
+    int currentTime = 0; 
+
+    int n = procs.size();
+    if(n==0) return;
+
+    int completedProcesses = 0;
+    vector<bool> isCompleted(n, false);
+
+    while(completedProcesses < n){
+        int shortestJobIndex = -1;
+        int minBurstTime = INT_MAX;
+        
+        // Encontra dentro dos processos aquele como menor tempo de execucao
+        for(int i=0; i<n; i++){
+            if(procs[i].arrivalTime <= currentTime && !isCompleted[i]){
+                if(procs[i].timeToLeave < minBurstTime){
+                    minBurstTime = procs[i].timeToLeave;
+                    shortestJobIndex = i;
+                }
+            }
+        }
+
+        if (shortestJobIndex != -1){
+            
+            // Calcula o tempo de resposta do processo
+            procs[shortestJobIndex].responseTime = currentTime - procs[shortestJobIndex].   arrivalTime;
+
+            // Adiciona ao tempo atual da CPU o tempo de execucao do processo
+            currentTime += procs[shortestJobIndex].timeToLeave;
+
+            // Calcula o tempo de retorno
+            procs[shortestJobIndex].returnTime = currentTime - procs[shortestJobIndex].   arrivalTime;
+
+            // Tempo de espera
+            procs[shortestJobIndex].waitTime = procs[shortestJobIndex].returnTime - procs[shortestJobIndex].timeToLeave;
+
+            isCompleted[shortestJobIndex] = true;
+            completedProcesses++;
+        }
+        else{
+            currentTime++;
+        }
+    }
+     showResults("SJF: ", procs);
 }
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,24 @@
+#include <fstream>
+#include "cpu-scheduler.hh"
+
 int main(int argc, char const *argv[])
 {
+    ifstream readFile("inputs/procs_2.txt");
+    CPUScheduler scheduler;
+    int arrivalTime, burst;
 
+    if(!readFile.is_open()){
+        cout << "Error\n\n";
+        return 0;
+    }
+
+    while(!readFile.eof()){
+        readFile >> arrivalTime >> burst;
+        scheduler.addProcess(arrivalTime, burst);        
+    }   
+
+    scheduler.SJF();
+
+    readFile.close(); 
     return 0;
 }
